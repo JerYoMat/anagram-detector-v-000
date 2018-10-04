cb19c01a49951336fbef38db67dcc389fbbcfd42
Jeremy Emata <jeremy.emata@gmail.com>
2018-10-04 08:34:31 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,9 +4,8 @@ require 'pry'
 class Anagram
   attr_accessor :string_to_check
   
-  def initialize(string)
-    @string_to_check = string 
-  end 
+
+
   
   def match 
     array = @string_to_check.split
